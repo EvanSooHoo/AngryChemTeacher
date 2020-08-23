6bbe8b3698569c4adfe16c8ad69dbbf057b207c5
evansoohoo <evan.soohoo@gmail.com>
2020-08-22 18:23:36 -0700

    centered submit button

--- angrychemteacher/src/App.vue
+++ angrychemteacher/src/App.vue
@@ -1,47 +1,49 @@
-<template>
-  
+  <template>
   <v-app>
-    <h3>Insert chem question here</h3>
-    <v-container fluid>
-    <v-textarea
-      v-model="inputAnswer"
-      clearable
-      clear-icon="cancel"
-      label="Text"
-      value="This is clearable text."
-    ></v-textarea>
-    <div class="my-2">
-        <!--<v-btn @click="buttonPressed()" small>Submit</v-btn>-->
-        <v-btn @click="buttonPressed" small>Submit</v-btn>
-
-      </div>
-  </v-container>
     
+    <p>Insert chem question here</p>
+    <v-card>
+      <v-container fluid>
+        <v-textarea
+          v-model="inputAnswer"
+          clearable
+          clear-icon="cancel"
+          label="Type your answer here"
+          value="This is clearable text."
+        ></v-textarea>
+
+        <!--<v-btn @click="buttonPressed()" small>Submit</v-btn>-->
+        <v-layout row>
+          <v-col cols="6" offset="5">
+            <div>
+              <v-btn @click="buttonPressed" small>Submit</v-btn>
+            </div>
+          </v-col>
+        </v-layout>
+      </v-container>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 export default {
-  
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
   methods: {
-      buttonPressed () {
-        this.clickCount++;
-        console.log('Button pressed. value of input answer is ' + this.inputAnswer);
-        console.log('Value of click count is ' + this.clickCount); 
-      },
-      
-    },
+    buttonPressed() {
+      this.clickCount++;
+      console.log(
+        "Button pressed. value of input answer is " + this.inputAnswer
+      );
+      console.log("Value of click count is " + this.clickCount);
+    }
+  },
 
   data: () => ({
     //
     inputAnswer: "",
     clickCount: 0
-
-    
-  }),
+  })
 };
 </script>
