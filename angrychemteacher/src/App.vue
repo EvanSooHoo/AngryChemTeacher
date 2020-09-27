653d70e99164f8d73377ac57825d5ee360d47790
evansoohoo <evan.soohoo@gmail.com>
2020-09-26 17:35:59 -0700

    added chem balancing section, with subscripts and text boxes

--- angrychemteacher/src/App.vue
+++ angrychemteacher/src/App.vue
@@ -1,8 +1,8 @@
-  <template>
+<template>
   <v-app>
     <v-card>
       <v-card-title primary-title class="justify-center">
-        <p>{{questionText}}</p>
+        <p>{{ questionText }}</p>
       </v-card-title>
       <v-container fluid>
         <v-textarea
@@ -27,6 +27,39 @@
         <v-textarea value="This is clearable text."></v-textarea>
       </v-container>
     </v-card>
+    <v-container>
+      <h1 class="text-center">Balance the chemical equation</h1>
+      <v-spacer></v-spacer>
+      <v-row>
+        <v-col cols="12" sm="6" md="2">
+          <v-text-field label="Outlined" outlined></v-text-field>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="2">
+          <h1>
+            {{ firstElement }}<sub>{{firstSubscript}}</sub>
+          </h1>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="2">
+          <v-text-field label="Outlined" outlined></v-text-field>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="2">
+          <h1>{{ secondElement }}<sub>{{secondSubscript}}</sub></h1>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="2">
+          <v-text-field label="Outlined" outlined></v-text-field>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="2">
+          <h1>
+            {{ productElement }}<sub>{{thirdSubscript}}</sub>
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -36,34 +69,23 @@ export default {
 
   components: {},
   methods: {
-    processName(inputName)
-    {
+    processName(inputName) {
       inputName = inputName.toLowerCase();
-      if (inputName === "kai") 
-      {
-        this.questionText = "KAI?!  GET OUT OF MY CLASS.  Just kidding.  You're a very good student.";
+      if (inputName === "kai") {
+        this.questionText =
+          "KAI?!  GET OUT OF MY CLASS.  Just kidding.  You're a very good student.";
         this.firstGrade = "A";
-      }
-      else if(inputName === "zaid")
-      {
+      } else if (inputName === "zaid") {
         this.questionText = "Zaid? Rock on";
-      }
-      else if(inputName === "15/25")
-      {
+      } else if (inputName === "15/25") {
         this.questionText = "Bonus activated";
-      }
-      else if(inputName === "david" || inputName === "jon")
-      {
+      } else if (inputName === "david" || inputName === "jon") {
         this.firstGrade = "A";
-      }
-      else if(inputName === "stanley")
-      {
+      } else if (inputName === "stanley") {
         this.firstGrade = "A+";
-      }
-      else 
-      {
+      } else {
         this.firstGrade = "B+";
-      }         
+      }
     },
     buttonPressed() {
       this.clickCount++;
@@ -72,17 +94,35 @@ export default {
       );
       console.log("Value of click count is " + this.clickCount);
       this.processName(this.inputAnswer);
-    },
-    
+      if (this.clickCount == 1) {
+        var fullStr =
+          this.inputAnswer +
+          ", come to the board. Prove that you deserve your " +
+          this.firstGrade;
+        this.questionText = fullStr;
+        this.firstElement = "Fe";
+        this.firstSubscript = "2";
+        this.secondElement = "Cl";
+        this.secondSubscript = "3";
+        this.productElement = "FeCl";
+        this.thirdSubscript = "3";
+      }
+    }
   },
 
   data: () => ({
     //
     inputAnswer: "",
     clickCount: 0,
-    questionText: "\nWelcome to Ay Nako! Enter your name and hit submit to proceed",
+    questionText:
+      "\nWelcome to Ay Nako! Enter your name and hit submit to proceed",
     gradeValue: "",
-
+    firstElement: "",
+    secondElement: "",
+    productElement: "",
+    firstSubscript: "",
+    secondSubscript: "",
+    thirdSubscript: ""
   })
 };
 </script>
