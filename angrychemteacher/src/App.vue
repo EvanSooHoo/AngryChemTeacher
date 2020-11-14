274929f526241e886e1dd5e9727397f27b5dda48
evansoohoo <evan.soohoo@gmail.com>
2020-11-13 16:00:07 -0800

    added do now parse

--- angrychemteacher/src/App.vue
+++ angrychemteacher/src/App.vue
@@ -4,8 +4,11 @@
       <v-card-title primary-title class="justify-center">
         <p>{{ questionText }}</p>
       </v-card-title>
-      <v-card-text primary-title class="justify-center headline font-weight-bold">
-        <center>{{questionTextLineTwo}}</center>
+      <v-card-text
+        primary-title
+        class="justify-center headline font-weight-bold"
+      >
+        <center>{{ questionTextLineTwo }}</center>
       </v-card-text>
       <v-container fluid>
         <v-textarea
@@ -65,7 +68,12 @@
         </v-col>
         <v-col cols="12" sm="6" md="12">
           <v-row justify="center">
-            <v-btn :disabled="lockBalanceButton" @click="balanceButtonPressed" small>Submit!</v-btn>
+            <v-btn
+              :disabled="lockBalanceButton"
+              @click="balanceButtonPressed"
+              small
+              >Submit!</v-btn
+            >
           </v-row>
         </v-col>
       </v-row>
@@ -79,6 +87,41 @@ export default {
 
   components: {},
   methods: {
+    parseDoNow(inputDN) {
+      inputDN = inputDN.toLowerCase();
+      inputDN = inputDN.trim();
+      this.score = 0;
+      if (inputDN.length >= 1) {
+          this.score += 50;
+      } else {
+          this.reason += "\nYou wrote nothing";
+      }
+      if (inputDN.search("neutron") != -1 && inputDN.seach("proton") != -1
+
+              && inputDN.search("electron") != -1) {
+
+          this.score += 40;
+
+      } else {
+
+          this.reason += "\n-You did not mention ALL THREE sub-atomic particles";
+
+      }
+
+      if ((inputDN.search("building") != -1)
+          || (inputDN.search("block") != -1 || inputDN.search("make") != -1)
+          || (inputDN.search("fundamental") != -1)
+          || (inputDN.search("important") != -1)) {
+          this.score += 10;
+
+      } else {
+
+          this.reason += "\n-You did not say it's the building block of all things";
+
+      }
+
+      
+    },
     processName(inputName) {
       inputName = inputName.toLowerCase();
       inputName = inputName.trim();
@@ -126,22 +169,26 @@ export default {
         this.productElement = "FeCl";
         this.lockBalanceButton = false;
       }
-      
+
       else if(this.clickCount == 3) {
         this.questionText = 'DO NOW';
-        this.questionTextLineTwo = 'This is a short answer question.  \nYou are to write everything you know about the atom.\n\nWhat is it made of?  \nWhat is its significance?\n\nYou will be given a score out of 100 based on your short answer response:';  
+        this.questionTextLineTwo = 'This is a short answer question.  \nYou are to write everything you know about the atom.\n\nWhat is it made of?  \nWhat is its significance?\n\nYou will be given a score out of 100 based on your short answer response:';
       }
-      
+      else if(this.clickCount == 4) {
+        this.parseDoNow(this.inputAnswer);
+        this.questionText = 'Your score is ' + this.score + ' because ' + this.reason;
+      }
+
     },
     balanceButtonPressed() {
-       var isCorrect = this.validateBalance(parseInt(this.firstCoeff), parseInt(this.secondCoeff), parseInt(this.thirdCoeff)); 
+       var isCorrect = this.validateBalance(parseInt(this.firstCoeff), parseInt(this.secondCoeff), parseInt(this.thirdCoeff));
        this.questionText = 'IS THAT THE ANSWER, CLASS?';
        if(isCorrect) {
-         
+
          this.questionTextLineTwo += 'YES';
        } else {
          this.questionTextLineTwo += 'NO';
-       }   
+       }
        this.questionTextLineTwo += ' (Click submit to continue)'
        this.lockBalanceButton = true;
     }
@@ -164,7 +211,9 @@ export default {
     thirdSubscript: "",
     firstCoeff: "",
     secondCoeff: "",
-    thirdCoeff: ""
+    thirdCoeff: "",
+    score: 0,
+    reason: ""
   })
 };
 </script>
